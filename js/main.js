83bf7e34159be472e95edeedb7b034fe5b8b0c78
Denys Sviridov <115045351+Denys89-eng@users.noreply.github.com>
2023-01-24 14:25:57 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,50 +9,71 @@ let form = document.querySelector('.form'),
     input = document.querySelector('.input'),
     header = document.querySelector('.header')
 
+function removeCard() {
+    const prevCard = document.querySelector('.card')
+    if (prevCard) prevCard.remove()
+}
 
-form.onsubmit = function (e) {
-    e.preventDefault();
-    let city = input.value.trim();
+function showError(errorMessage) {
+    const html = `<div class="card">${data.error.message}</div>`
+    header.insertAdjacentHTML('afterend', html)
+}
 
-    const url = ` http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data)
-
-
-            if (data.error) {
-
-                const prevCard = document.querySelector('.card')
-                if (prevCard) prevCard.remove()
-
-                const html = `<div class="card">${data.error.message}</div>`
-                header.insertAdjacentHTML('afterend', html)
-
-            } else {
-                const prevCard = document.querySelector('.card')
-                if (prevCard) {
-                    prevCard.remove()
-                }
-
-
-                let html = `<div class="card">
-                    <h2 class="card-city">${data.location.name}<span>${data.location.country}</span></h2>
+function showCard({name, country, temp, text}) {
+    let html = `<div class="card">
+                    <h2 class="card-city">${name}<span>${country}</span></h2>
                 
                     <div class="card-weather">
-                        <div class="card-value">${data.current.temp_c}°C</div>
+                        <div class="card-value">${temp}°C</div>
                         <div class="card-image"><img src="img/8.png" alt=""></div>
                     </div>
                 
-                    <div class="card-descr">${data.current.condition.text}</div>
+                    <div class="card-descr">${text}</div>
                 
                 </div>`
 
-                header.insertAdjacentHTML('afterend', html)
-            }
-        })
+    header.insertAdjacentHTML('afterend', html)
 }
 
+async function getWeather(city) {
+    const url = ` http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+form.onsubmit = async function (e) {
+    e.preventDefault();
+
+    let city = input.value.trim();
+
+    const data = await getWeather(city)
+
+    if (data.error) {
+
+        removeCard()
+        showError(data.error.message)
+
+    } else {
+
+        removeCard()
+
+        const weatherData = {
+            name: data.location.name,
+            country: data.location.country,
+            temp: data.current.temp_c,
+            text: data.current.condition.text
+        }
+
+        showCard(weatherData)
+
+    }
+}
+
+
+// const info = conditions.find(
+//     (obj) => obj.code === data.current.condition.code
+// );
+//
+//  const fileName = data.current.is_day ? info.day : info.night;
+//  console.log('fileName', fileName)
